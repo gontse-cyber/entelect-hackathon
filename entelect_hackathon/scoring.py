d1bfa11ed6f1299266835f44d6c04e4731dcef67
Sicelo Mkhatshwa <sicelomkhatshwa1705@gmail.com>
2026-04-18 12:33:01 +0200

    Update scoring.py

--- entelect_hackathon/scoring.py
+++ entelect_hackathon/scoring.py
@@ -1,5 +1,1 @@
-def calculate_score(total_time: float, reference_time: float) -> float:
-    """Calculate Level 1 score"""
-    if total_time <= 0:
-        return 0
-    return 500000 * (reference_time / total_time) ** 3
+
